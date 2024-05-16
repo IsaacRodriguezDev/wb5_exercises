8c210199ed3eca120b916a5a53a791f9ace0eecd
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-05-16 15:50:51 -0500

    completed food_menu_array.js

---
+++ multiArraysScript/food_menu_array.js
@@ -0,0 +1,41 @@
+let menu = [
+  [
+    { item: "Sausage and Egg Biscuit", price: 3.69 },
+    { item: "Bacon and Egg Biscuit", price: 3.49 },
+    { item: "Ham and Egg Biscuit", price: 3.29 },
+  ],
+  [
+    { item: "Pepperoni Pizza", price: 3.69 },
+    { item: "Philly Cheesesteak", price: 3.49 },
+    { item: "Cheese Burger", price: 3.29 },
+    { item: "Korean Fried Chicken", price: 28.29 },
+  ],
+  [
+    { item: "Orange Chicken", price: 10.69 },
+    { item: "Chicken Alfredo", price: 12.49 },
+    { item: "Chicken Pot Pie", price: 10.29 },
+    { item: "Pasta", price: 11.29 },
+    { item: "All you can eat salad bar", price: 12.29 },
+  ],
+];
+
+
+let meal = 0
+
+if(meal === 0){
+    console.log('Breakfast Menu')
+    console.log('---------------')
+}
+if(meal === 1){
+    console.log('Lunch Menu')
+    console.log('---------------')
+}
+if(meal === 2){
+    console.log('Dinner Menu')
+    console.log('---------------')
+}
+
+menu[meal].forEach((menuItem)=>{
+console.log(`${menuItem.item}: ${menuItem.price}`)
+
+})
